b661a136768e158cf6ccf3869213d6937f9c1545
Marcio Carolino <marciocarolino@gmail.com>
2018-03-10 21:08:39 -0300

    Agora esta indo

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 <?php
 
 echo "Meu Primeiro Código <strong>PHP</strong>!";
 
-=======
-<?php
 
-echo "Meu Primeiro Código <strong>PHP</strong>!";
-
->>>>>>> 4e61de9f0794f4a8ad3d9a0edc9217189b13b107
 ?>
