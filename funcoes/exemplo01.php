ef6e0e7e2c32fbca189e4b63d332f91e1242395f
Marcio Carolino <marciocarolino@gmail.com>
2018-03-28 21:07:26 -0300

    Continuandoa aula

--- funcoes/exemplo01.php
+++ funcoes/exemplo01.php
@@ -8,6 +8,6 @@ function ola(){
   
 }
 
-echo ola();
+echo 	ola();
 
 ?>
---
+++ funcoes/exemplo03.php
@@ -0,0 +1,14 @@
+<?php
+
+function ola($texto){
+
+	return "Ola $texto! <br>";
+
+
+}
+
+echo ola("mundo");
+echo ola("meu povo");
+echo ola("Como estamos!!!");
+
+?>
